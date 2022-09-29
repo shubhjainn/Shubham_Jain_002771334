fef06d1892579ce1bcb50353b6310ed535559e0f
shubhjainn <jain.shu@northeastern.edu>
2022-09-29 14:39:54 -0400

    working

--- Assingment1/src/main/java/com/neu/assingment1/my_package/EmployeeManagementPortal.java
+++ Assingment1/src/main/java/com/neu/assingment1/my_package/EmployeeManagementPortal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package my_package;
+package com.neu.assingment1.my_package;
 
 import javax.swing.table.DefaultTableModel;
 import model.AddEmployee;
