2e17adf982b11f55bc38f34d51984fa5b622037a
shubhjainn <jain.shu@northeastern.edu>
2022-09-29 12:32:00 -0400

    Initial Commit

---
+++ src/CreateEmployeeScreen.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class CreateEmployeeScreen {
+    private JPanel main_panel;
+    private JButton addEmployeePortal;
+    private JButton editEmployeeButton;
+    private JButton deleteEmployeeButton;
+    private JLabel mainPageHeading;
+}
